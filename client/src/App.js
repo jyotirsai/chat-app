26822e0de27795c7bafad8d373d93f5e5921a783
jyotirsai <jyotirsai28@gmail.com>
2020-11-08 20:49:25 -0700

    added sockets

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
+import React, {useState, useEffect} from "react";
+import io from "socket.io-client";
 import './App.css';
 import Chat from './components/Chat/Chat'
 import Score from './components/Score/Score'
 import Live from './components/Live/Live'
 import Poll from './components/Poll/Poll'
 
+const ENDPOINT = "http://localhost:5000"
+
 function App() {
+
+  useEffect(() => {
+    const socket = io(ENDPOINT);
+    console.log(socket);
+  })
+
   return (
     <div className="App">
       <div className="position">
